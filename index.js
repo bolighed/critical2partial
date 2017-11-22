1de01c1cb58c3639657687ca3f153d9d07bced17
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2017-11-22 15:37:32 +0100

    support url

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const critical = require('critical');
 const fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
 
 module.exports = (CONFIG) => {
     const generateCritical = function(url, file_path) {
-        const body = fs.readFileSync(path.join(__dirname, url), 'utf8');
-        
-        Object.assign(CONFIG.critical_options, {html: body});
-    
+
+        if (url.startsWith("http")) {
+            fetch(url).then((data) => {
+                return data.text()
+            }).then((body) => {
+                Object.assign(CONFIG.critical_options, {html: body});
+                generateCritical(CONFIG)
+            })
+        } else {
+            const body = fs.readFileSync(path.join(__dirname, url), 'utf8');
+            Object.assign(CONFIG.critical_options, {html: body});
+            generateCritical(CONFIG)
+        }
+
+    }
+
+    const genereteCritical = function(CONFIG) {
         critical.generate(CONFIG.critical_options).then(function (output) {
             output = '<style>'+output+'</style>';
             const output_file_path = path.join(__dirname, file_path);
